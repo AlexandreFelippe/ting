d44266ce97f0f91fe837294a71b2e3c963e6566c
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-11 09:34:36 -0300

    Implementa Requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.lower().endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file, "r", encoding="utf-8") as file:
+            lines = file.read().split("\n")
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+    except ValueError as ve:
+        print(ve, file=sys.stderr)
+        return []
