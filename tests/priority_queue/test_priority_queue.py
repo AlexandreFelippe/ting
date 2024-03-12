5c08cc6598186652a66715df093bcd18dd1440a0
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-12 14:24:48 -0300

    Requisito 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,25 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
-# import pytest
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
 
-    # my_priority_queue = PriorityQueue()
+    my_priority_queue = PriorityQueue()
 
-    # my_priority_queue.enqueue({"qtd_linhas": 3})
-    # my_priority_queue.enqueue({"qtd_linhas": 4})
-    # my_priority_queue.enqueue({"qtd_linhas": 6})
+    my_priority_queue.enqueue({"qtd_linhas": 3})
+    my_priority_queue.enqueue({"qtd_linhas": 4})
+    my_priority_queue.enqueue({"qtd_linhas": 6})
 
-    # assert len(my_priority_queue) == 3
-    # assert len(my_priority_queue.high_priority) == 2
-    # assert len(my_priority_queue.regular_priority) == 1
+    assert len(my_priority_queue) == 3
+    assert len(my_priority_queue.high_priority) == 2
+    assert len(my_priority_queue.regular_priority) == 1
 
-    # my_priority_queue.dequeue()
-    # assert len(my_priority_queue) == 2
+    my_priority_queue.dequeue()
+    assert len(my_priority_queue) == 2
 
-    # searched_element = my_priority_queue.search(0)
-    # assert searched_element["qtd_linhas"] == 4
+    searched_element = my_priority_queue.search(0)
+    assert searched_element["qtd_linhas"] == 4
 
-    # with pytest.raises(IndexError):
-    #     my_priority_queue.search(52)
+    with pytest.raises(IndexError):
+        my_priority_queue.search(52)
