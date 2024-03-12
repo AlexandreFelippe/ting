628388bacdf35e323e71610a8d8db49c842c38ac
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-12 13:59:05 -0300

    Implementa Requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,39 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for file_data in instance.data:
+        filename = file_data["nome_do_arquivo"]
+        occurrences = []
+
+        for index, line in enumerate(file_data["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index})
+
+        if occurrences:
+            results.append({
+                "palavra": word.lower(),
+                "arquivo": filename,
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for file_data in instance.data:
+        filename = file_data["nome_do_arquivo"]
+        occurrences = []
+
+        for index, line in enumerate(file_data["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index, "conteudo": line})
+
+        if occurrences:
+            results.append({
+                "palavra": word.lower(),
+                "arquivo": filename,
+                "ocorrencias": occurrences
+            })
+    return results
